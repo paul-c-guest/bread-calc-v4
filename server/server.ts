1fbb8da4818fee4d5dac8f970342faaad1ae1c13
Paul Guest <paul.christopher.guest@gmail.com>
2023-09-13 18:37:58 +1200

    add files and settings for dokku deployment

--- server/server.ts
+++ server/server.ts
@@ -14,10 +14,11 @@ server.use("/api/v1/flours", flourRoutes)
 server.use("/api/v1/users", userRoutes)
 server.use("/api/v1/overrides", overrideRoutes)
 
-if (process.env.NODE_ENV === "production") {
-  server.use("/assets", express.static(Path.resolve(__dirname, "../assets")))
-  server.get("*", (req, res) => {
-    res.sendFile(Path.resolve(__dirname, "../index.html"))
+if (process.env.NODE_ENV === 'production') {
+  server.use(express.static(Path.resolve('public')))
+  server.use('/assets', express.static(Path.resolve('./dist/assets')))
+  server.get('*', (req, res) => {
+    res.sendFile(Path.resolve('./dist/index.html'))
   })
 }
 
