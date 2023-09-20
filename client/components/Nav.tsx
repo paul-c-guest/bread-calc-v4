28d0297824caea21793d77e803b05d904b0894d5
Paul Guest <paul.christopher.guest@gmail.com>
2023-09-21 07:34:25 +1200

    add media query css rules where required, switch sides for nav and login blocks

--- client/components/Nav.tsx
+++ client/components/Nav.tsx
@@ -13,25 +13,25 @@ export function Nav() {
   return (
     <nav>
       <h1 id="title">Good Leavening</h1>
-      <div className="login-block">
-        <button className="login-button" onClick={handleLogin}>
-          {isAuthenticated ? "Logout" : "Login"}
-        </button>
-        {isAuthenticated && (
-          <div className="welcome-string">
-            Welcome, {user?.given_name ?? user?.nickname}
-          </div>
+      <div className="nav-button-block">
+        {isAuthenticated && location.pathname !== "/flours" && (
+          <button className="nav-button" onClick={() => navigate("flours")}>
+            Flours
+          </button>
         )}
-        <div className="nav-button-block">
-          {isAuthenticated && location.pathname !== "/flours" && (
-            <button className="nav-button" onClick={() => navigate("flours")}>
-              Flours
-            </button>
-          )}
-          {isAuthenticated && location.pathname !== "/" && (
-            <button className="nav-button" onClick={() => navigate("/")}>
-              Selections
-            </button>
+        {isAuthenticated && location.pathname !== "/" && (
+          <button className="nav-button" onClick={() => navigate("/")}>
+            Selections
+          </button>
+        )}
+        <div className="login-block">
+          <button className="login-button" onClick={handleLogin}>
+            {isAuthenticated ? "Logout" : "Login"}
+          </button>
+          {isAuthenticated && (
+            <div className="welcome-string">
+              Welcome, {user?.given_name ?? user?.nickname}
+            </div>
           )}
         </div>
       </div>
