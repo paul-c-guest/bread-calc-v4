04afcf2fb89bf424b9f83efd18cbcdffee7bf0a6
Paul Guest <paul.christopher.guest@gmail.com>
2023-10-02 13:00:25 +1300

    change deployment to normal updates [no knex actions]

--- knex-init.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-set -ex
-npm run knex:init
-npm run start
---
+++ startup.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+set -ex
+# npm run knex:init # uncomment to run knex migrations and seeds with every spin-up
+npm run start
